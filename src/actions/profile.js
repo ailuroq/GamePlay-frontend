32243b52aa9c92b4ac6771600276505db5e52fa4
ailuroq <yaroslav_ovsyannikov@list.ru>
2021-05-20 23:21:09 +0300

    avatar

--- src/actions/profile.js
+++ src/actions/profile.js
@@ -1,4 +1,4 @@
-import {GET_PROFILE} from "./types";
+import {GET_PROFILE, UPDATE_USER_AVATAR} from "./types";
 import axios from "axios";
 import authHeader from "../services/auth-header";
 import {API_URL} from "../constants/urlConstants";
@@ -9,7 +9,7 @@ const _getProfile = (profileData) => ({
 })
 
 
-const getProfile = (username) => {
+export const getProfile = (username) => {
     return (dispatch) => {
         return axios
             .get(API_URL + username, {headers: authHeader()})
@@ -21,5 +21,27 @@ const getProfile = (username) => {
             })
     }
 }
-export default getProfile
+
+const _updateAvatar = data => ({
+    type: UPDATE_USER_AVATAR,
+    payload: data
+})
+
+export const updateAvatar = (avatar) => {
+    return dispatch => {
+        const formData = new FormData()
+        formData.append('image', avatar)
+        const user = JSON.parse(localStorage.getItem("user"));
+        return axios
+            .post(API_URL + 'upload/avatar/', formData,{
+                headers: {
+                    'Authorization': `Bearer ${user.accessToken}`,
+                    'Content-Type': 'form-data'
+                },
+            })
+            .then(result => {
+                dispatch(_updateAvatar(result.data))
+            })
+    }
+}
 
--- src/components/Content/Profile/Profile.js
+++ src/components/Content/Profile/Profile.js
@@ -1,22 +1,41 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from 'react-router-dom';
 import {useDispatch, useSelector} from "react-redux";
 import styles from "./Profile.module.css"
 import {useParams} from "react-router-dom";
-import getProfile from "../../../actions/profile";
+import {getProfile, updateAvatar} from "../../../actions/profile";
 import avatar from "../../../static/avatar.jpeg"
 import {API_URL} from "../../../constants/urlConstants";
+import Button from "@material-ui/core/Button";
+import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@material-ui/core";
 
 const Profile = () => {
-    const {user: currentUser} = useSelector((state)=> state.auth)
+    const {user: currentUser} = useSelector((state) => state.auth)
     const {username} = useParams()
-
+    const [avatarDialog, setAvatarDialog] = useState(false)
+    const [photo, setPhoto] = useState()
+    const [disable, setDisable] = useState(true)
     const dispatch = useDispatch()
 
-    useEffect(()=> {
+    useEffect(() => {
         dispatch(getProfile(username))
     }, [dispatch, username])
 
+    const handlePhotoChange = (e) => {
+        const photo = e.target.files[0]
+        setDisable(false)
+        setPhoto(photo)
+    }
+    const handleAlertDialogOpen = () => {
+        setAvatarDialog(true)
+    }
+    const handleAlertDialogClose = () => {
+        setAvatarDialog(false)
+    }
+    const handlePhotoSubmit = () => {
+        console.log(photo)
+        dispatch(updateAvatar(photo))
+    }
     const {profileData} = useSelector(state => state.profile)
 
     return (
@@ -27,37 +46,53 @@ const Profile = () => {
                     <img src={API_URL + 'uploads/' + profileData.avatarName} className={styles.avatar} alt=""/>
                     }
                 </div>
+                <div><Button onClick={handleAlertDialogOpen} variant='contained'>Изменить аватар</Button></div>
             </div>
+            <Dialog
+                open={avatarDialog}
+                onClose={handleAlertDialogClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">{"Возможные нежелательные удаления данных"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Выбрать новый аватар
+                    </DialogContentText>
+                    <input name='image' onChange={handlePhotoChange} type="file"/>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleAlertDialogClose} color="primary">
+                        Отмена
+                    </Button>
+                    <Button
+                        disabled={disable}
+                        onClick={() => {
+                            handleAlertDialogClose()
+                            handlePhotoSubmit()
+                            window.location.reload(true);
+                        }}
+                        color="primary" autoFocus>
+                        Изменить
+                    </Button>
+                </DialogActions>
+            </Dialog>
             <div className={styles.wide_column}>
                 <div>
                     <div className={styles.user_info}>
                         <div className={styles.user_info_top}>
                             <p>{profileData.username}</p> {/*Username from the server*/}
                         </div>
-                        <div>
-                            <p>Last online</p> {/*Last online time from the server*/}
-                        </div>
                     </div>
                     <div className={styles.basic_info}>
                         <div className={styles.basic_info_top}>
                             <p>Basic info</p>
                             <p>Email: {profileData.email}</p>
-                            <Link to={"/" + currentUser.username + "/subscribers"}>Subscribers: {profileData.subscribers.length}</Link>
+                            <Link
+                                to={"/" + currentUser.username + "/subscribers"}>Subscribers: {profileData.subscribers.length}</Link>
                         </div>
                     </div>
-                    <div className={styles.favorite_games}>
-                        <div className={styles.favorite_games_text}>
-                            <p>Favorite games</p>
-                        </div>
-                        <div className={styles.games_pict}>
-                            <div><img src={avatar} alt=""/></div>
-                            <div><img src={avatar} alt=""/></div>
-                            <div><img src={avatar} alt=""/></div>
-                            <div><img src={avatar} alt=""/></div>
-                            <div><img src={avatar} alt=""/></div>
-                            <div><img src={avatar} alt=""/></div>
-                        </div>
-                    </div>
+
                     <div className={styles.achievements}>
                         <div/>
                     </div>
